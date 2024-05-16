be0e69ce94608ba33f5073a28e69f9c053de7452
Maksymilian Polak <mpolak@edu.cdv.pl>
2024-05-16 12:59:17 +0200

    added script

---
+++ FlappyBird/Assets/BirdController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+
+    public Rigidbody2D rb2D;
+    public Vector2 JumpForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello, world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb2D.AddForce(JumpForce, ForceMode2D.Impulse);
+
+        }
+
+    }
+}
